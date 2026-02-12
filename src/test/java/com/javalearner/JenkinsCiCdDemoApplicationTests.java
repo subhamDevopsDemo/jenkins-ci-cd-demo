dbd7ff2ccd3fe297780eeb769a69ba240d185dd7
Subham <subhammohanty44@gmail.com>
2026-02-12 11:59:09 +0530

    Pom File Changed and Test Cases Added

--- src/test/java/com/javalearner/JenkinsCiCdDemoApplicationTests.java
+++ src/test/java/com/javalearner/JenkinsCiCdDemoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.javalearner;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 class JenkinsCiCdDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	public void testDemo() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/jenkins/greetings/{name}", "Subham"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
