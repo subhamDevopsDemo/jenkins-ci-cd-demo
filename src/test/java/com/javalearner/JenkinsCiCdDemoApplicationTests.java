fed175131578025b91487b76b4740340ceeeb6f5
Subham <subhammohanty44@gmail.com>
2026-02-12 12:06:09 +0530

    Test Cases Issue Fixed

--- src/test/java/com/javalearner/JenkinsCiCdDemoApplicationTests.java
+++ src/test/java/com/javalearner/JenkinsCiCdDemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.javalearner;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class JenkinsCiCdDemoApplicationTests {
 
 	@Autowired
@@ -15,7 +17,7 @@ class JenkinsCiCdDemoApplicationTests {
 
 	@Test
 	public void testDemo() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/jenkins/greetings/{name}", "Subham"))
+		mockMvc.perform(MockMvcRequestBuilders.get("/greetings/{name}", "Subham"))
 				.andExpect(MockMvcResultMatchers.status().isOk());
 	}
 }
